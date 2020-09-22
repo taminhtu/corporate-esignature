5719692565bd522fcdfecf4ae9fff8d143f06fd9
Ta Minh Tu <taminhtu@gmail.com>
2020-09-22 17:40:00 +0700

    commit

---
+++ src/main/java/com/tcb/corp/esignature/CorporateEsignatureApplication.java
@@ -0,0 +1,13 @@
+package com.tcb.corp.esignature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorporateEsignatureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorporateEsignatureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tcb/corp/esignature/CorporateEsignatureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcb.corp.esignature;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorporateEsignatureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
